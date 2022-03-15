0a87fafdbfe1fd2c666f617902e57d91af9cd57d
nisylk <nisylk@IFSCLIENT-62464>
2022-03-15 20:46:49 +0530

    Add project files.

---
+++ BillingKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BillingKata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
